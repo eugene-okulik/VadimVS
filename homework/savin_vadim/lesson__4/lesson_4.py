86e7f3aee6edda3583975cc190d858683a5de0a5
Vadim <vadimarmo@gmail.com>
2025-03-02 16:36:30 +0300

    lesson_4 iteration8

--- homework/savin_vadim/lesson__4/lesson_4.py
+++ homework/savin_vadim/lesson__4/lesson_4.py
@@ -30,5 +30,5 @@ last_items = my_dict["dictt"].pop("textt")
 
 # добавьте новый элемент в множество
 my_dict["sett"].add("new")
-# удалите элемент из множествa
+# удалите элемент из множества
 my_dict["sett"].remove(True)
