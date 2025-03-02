abf15e38642cf94a5d531e6c7a9b7a23dcb7d367
Vadim <vadimarmo@gmail.com>
2025-03-02 16:04:52 +0300

    lesson_4 iteration2

--- homework/savin_vadim/lesson__4/lesson_4.py
+++ homework/savin_vadim/lesson__4/lesson_4.py
@@ -1,16 +1,32 @@
-my_dict = {"tuplee":(1, "ttuple", False, 4.42, 11),
-           "listt":[2,22,"llist", True,"я смогу", 2.22],
-           "dictt":{"intt":3,"textt":"ddict","bool_first":False, "bool_second":True,"floatt":3.33},
-           "sett":{4,44,True,"apple", 4.44}}
+my_dict = {"tuplee": (1, "ttuple", False, 4.42, 11),
+           "listt": [2, 22, "llist", True, "я смогу", 2.22],
+           "dictt": {
+                "intt": 3,
+                "textt": "ddict",
+                "bool_first": False,
+                "bool_second": True,
+                "floatt": 3.33,
+                },
+           "sett": {4, 44, True, "apple", 4.44}}
+
+
+# выведите на экран последний элемент
 print(my_dict["tuplee"][-1])
 
+
+# добавьте в конец списка еще один элемент
 my_dict["listt"].append("last_element")
+#удалите второй элемент списка
 my_dict["listt"].pop(1)
 
 
+# добавьте элемент с ключом ('i am a tuple',) и любым значением
 my_dict["dictt"]["i am a tuple"] = "странное название кортеж конечно"
+# удалите какой-нибудь элемент
 last_items = my_dict["dictt"].pop("textt")
-print(my_dict["dictt"])
 
-# print(my_dict["dictt"][-1])
-print(type(my_dict["sett"]))
+
+# добавьте новый элемент в множество
+my_dict["sett"].add("new")
+# удалите элемент из множества
+my_dict["sett"].remove(True)
