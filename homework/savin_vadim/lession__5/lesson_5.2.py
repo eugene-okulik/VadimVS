9e6b08eab1d38a0105d6048d15e815da52da1b4b
Vadim <vadimarmo@gmail.com>
2025-03-03 19:08:42 +0300

    lesson_5.2 iteration4

--- homework/savin_vadim/lession__5/lesson_5.2.py
+++ homework/savin_vadim/lession__5/lesson_5.2.py
@@ -3,7 +3,7 @@ result_one = "результат операции: 42"
 result_two = "результат операции: 514"
 result_programm = "результат работы программы: 9"
 
-# Можно сделать по действиям, 
+# Можно сделать по действиям,
 # но попытался поломать голвоу над однострочным решением
 print(int((result_one[(result_one.index(":")) + 1:]).lstrip()) + 10)
 print(int((result_two[(result_two.index(":")) + 1:]).lstrip()) + 10)
