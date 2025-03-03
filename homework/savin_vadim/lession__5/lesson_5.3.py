71b61c2d0a5ecb0e617071cebe9b76b489173b91
Vadim <vadimarmo@gmail.com>
2025-03-03 18:49:43 +0300

    lesson_5.3 iteration1

---
+++ homework/savin_vadim/lession__5/lesson_5.3.py
@@ -0,0 +1,14 @@
+# Задание 3
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+students_one, students_two, students_tree = students
+subjects_one, subjects_two, subjects_tree = subjects
+
+# example f-string
+print(f"Students {students_one}, {students_two}, {students_tree} study these subjects: {subjects_one}, {subjects_two}, {subjects_tree}")
+
+# example string format
+students_msg = ", ".join(students)
+subjects_msg = ", ".join(subjects)
+my_text = "Students {0} study these subjects: {1}"
+print(my_text.format(students_msg, subjects_msg))
