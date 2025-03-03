d964095235a188a20c05df6420c6f95ccc94c7f5
Vadim <vadimarmo@gmail.com>
2025-03-03 19:15:42 +0300

    lesson_5.3 iteration4

--- homework/savin_vadim/lession__5/lesson_5.3.py
+++ homework/savin_vadim/lession__5/lesson_5.3.py
@@ -1,10 +1,11 @@
 # Зaдание 3.
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
-students_one, students_two, students_tree = students
-subjects_one, subjects_two, subjects_tree = subjects
+
 
 # example f-string
+students_one, students_two, students_tree = students
+subjects_one, subjects_two, subjects_tree = subjects
 print(f"Students {students_one}, {students_two}, {students_tree} "
       f"study these subjects: {subjects_one}, {subjects_two}, {subjects_tree}")
 
