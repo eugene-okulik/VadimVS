bafa1f3477a483aeda382ec319a24d0bfb6c1712
Vadim <vadimarmo@gmail.com>
2025-03-06 18:43:58 +0300

    lesson_7.3

---
+++ homework/savin_vadim/lesson__7/lesson_7.3.py
@@ -0,0 +1,8 @@
+def integer_from_text(txt):
+    print(int(txt.split(":")[-1])+10)
+
+
+integer_from_text("результат операции: 42")
+integer_from_text("результат операции: 54")
+integer_from_text("результат работы программы: 209")
+integer_from_text("результат: 2")
