e4bf0a16b5e88357d0fdaef012b868ec6e0a45bc
Vadim <vadimarmo@gmail.com>
2025-03-09 02:35:44 +0300

    lesson_8.1

--- homework/savin_vadim/lesson__8/lesson_8.1.py
+++ homework/savin_vadim/lesson__8/lesson_8.1.py
@@ -5,7 +5,7 @@ def salary_bonus():
     salary = int(input("Введи сумму: "))
     bonus = random.choice([True, False])
     if bonus is True:
-        salary_new = salary + random.randint(500, 3000)
+        salary_new = salary + random.randrange(500, 3000, 50)
     else:
         salary_new = salary
     print(f"{salary}, {bonus} - '${salary_new}'")
