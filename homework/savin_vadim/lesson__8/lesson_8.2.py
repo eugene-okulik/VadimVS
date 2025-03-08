4280aca79b4fd344be5a9ce7a79ba2e5dd9ddafd
Vadim <vadimarmo@gmail.com>
2025-03-09 02:38:51 +0300

    lesson_8.1 и lesson_8.2

--- homework/savin_vadim/lesson__8/lesson_8.2.py
+++ homework/savin_vadim/lesson__8/lesson_8.2.py
@@ -1,6 +1,3 @@
-import time
-
-
 def fibo(iteration):
     first_int, second_int = 0, 1
     count = 0
@@ -17,8 +14,9 @@ def data_fibo_count(iteration):
     for number in fibo(iteration):
         if count == iteration:
             print(f"{iteration}-е число Фибоначчи: {number}")
-            break  
+            break
         count += 1
+
 
 data_fibo_count(10)
 data_fibo_count(200)
