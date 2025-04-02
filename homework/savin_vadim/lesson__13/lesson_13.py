b21b49162f7e6871bc305b47d6a9c72e4e1cc071
Vadim <vadimarmo@gmail.com>
2025-04-03 00:54:03 +0300

    lesson_13 iteration 2

--- homework/savin_vadim/lesson__13/lesson_13.py
+++ homework/savin_vadim/lesson__13/lesson_13.py
@@ -16,18 +16,16 @@ def read_file():
 line = read_file()
 
 for data_line in line:
-    with open(new_file_path, 'a') as new_file:
-        split_text = data_line.split()
-        date = split_text[1] + " " + split_text[2]  # Вытаскиваем "2023-11-27 20:34:13.212967"
-        date_parcing = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
-        if data_line.startswith("1."):
-            data_type_date1 = date_parcing + datetime.timedelta(days=7)
-            result_date_line1 = datetime.datetime.strftime(data_type_date1, "%Y-%m-%d %H:%M:%S.%f")
-            new_file.write(result_date_line1 + "\n")
-        elif data_line.startswith("2."):
-            result_date_line2 = datetime.datetime.strftime(date_parcing, "%A")
-            new_file.write(result_date_line2 + "\n")
-
-        elif data_line.startswith("3."):
-            data_type_date3 = (datetime.datetime.now() - date_parcing).days
-            new_file.write(str(data_type_date3))
+    split_text = data_line.split()
+    date = split_text[1] + " " + split_text[2]  # Вытаскиваем "2023-11-27 20:34:13.212967"
+    date_parcing = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
+    if data_line.startswith("1."):
+        data_type_date1 = date_parcing + datetime.timedelta(days=7)
+        result_date_line1 = datetime.datetime.strftime(data_type_date1, "%Y-%m-%d %H:%M:%S.%f")
+        print(result_date_line1)
+    elif data_line.startswith("2."):
+        result_date_line2 = datetime.datetime.strftime(date_parcing, "%A")
+        print(result_date_line2)
+    elif data_line.startswith("3."):
+        data_type_date3 = (datetime.datetime.now() - date_parcing).days
+        print(data_type_date3)
