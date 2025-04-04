bb62bdae8ba00d3419b03af8afe98f03bdd877c7
Vadim <vadimarmo@gmail.com>
2025-04-04 11:37:02 +0300

    lesson_14 iter 2

--- homework/savin_vadim/lesson__14/lesson_14.sql
+++ homework/savin_vadim/lesson__14/lesson_14.sql
@@ -1,27 +1,26 @@
-insert into students (id, name, second_name, group_id) VALUES (53 ,'Rulon', 'Oboev', 2)
-insert into books (title, taken_by_student_id) VALUES ('The Hitchhikers Guide to the Galaxy', 53)
-insert into books (title, taken_by_student_id) VALUES ('Harry Potter', 53)
-insert into books (title, taken_by_student_id) VALUES ('Mémoires de la vie privée de Benjamin Franklin', 53)
+insert into `groups` (title, start_date, end_date) VALUES ('activist', 'mar 2025', 'may 2025')
 
-insert into `groups` (id, title, start_date, end_date) VALUES (2, 'activist', 'mar 2025', 'may 2025')
+insert into students (name, second_name, group_id) VALUES ('Rulon', 'Oboev')
+
+insert into books (title, taken_by_student_id) VALUES ('The Hitchhikers Guide to the Galaxy', 20079)
+insert into books (title, taken_by_student_id) VALUES ('Harry Potter', 20079)
+insert into books (title, taken_by_student_id) VALUES ('Mémoires de la vie privée de Benjamin Franklin', 20079) 
 
 insert into subjets (title) VALUES ('Savin AQA')
 insert into subjets (title) VALUES ('Savin SQL')
 insert into subjets (title) VALUES ('Savin Python')
 
-
 insert into lessons  (title, subject_id) VALUES ('SQL_svs', 10044)
 insert into lessons  (title, subject_id) VALUES ('AQA_svs', 10043)
 insert into lessons  (title, subject_id) VALUES ('Python_svs', 10045)
 
-insert into marks (value, lesson_id , student_id ) VALUES (10, 9391, 53)
-insert into marks (value, lesson_id, student_id) VALUES ('9', 9392, 53)
-insert into marks (value, lesson_id, student_id) VALUES ('8', 9393, 53)
+insert into marks (value, lesson_id , student_id ) VALUES ('10', 9391, 20079)
+insert into marks (value, lesson_id, student_id) VALUES ('9', 9392, 20079)
+insert into marks (value, lesson_id, student_id) VALUES ('8', 9393, 20079)
 
+select * from marks m WHERE student_id = 20079
 
-select * from marks m WHERE student_id = 53
-
-select * from books WHERE taken_by_student_id  = 53
+select * from books WHERE taken_by_student_id  = 20079
 
 SELECT s.id as student_id, 
        s.name, 
@@ -44,5 +43,5 @@ LEFT JOIN books b ON s.id = b.taken_by_student_id
 LEFT JOIN marks m ON s.id = m.student_id
 LEFT JOIN lessons l ON m.lesson_id = l.id
 LEFT JOIN subjets s2 ON s2.id = l.subject_id
-WHERE s.id = 53
+WHERE s.id = 20079
 GROUP BY s.id, s.name, s.second_name, g.id, g.title, g.start_date, g.end_date
