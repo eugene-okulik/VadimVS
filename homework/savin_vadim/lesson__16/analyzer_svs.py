5d0b1b59a6b9d19e950ac2e9b32779c1bd8c0e58
Vadim <vadimarmo@gmail.com>
2025-04-13 00:36:24 +0300

    lesson_argparse with match

--- homework/savin_vadim/lesson__16/analyzer_svs.py
+++ homework/savin_vadim/lesson__16/analyzer_svs.py
@@ -16,30 +16,33 @@ with open(args.path) as open_file:
         if args.search in line:
             try:
                 position = line.index(args.search)
-                if position > 4 and position + 5 > len(line):
-                    future_row = next(open_file, None)
-                    if future_row is not None:
-                        future_index = abs(len(line) - (position + 5))
-                        result = " ".join(line[position - 5:] + (future_row.strip()).split()[:future_index])
+                
+                match position:
+                    case _ if position > 4 and position + 5 > len(line):  # Если требуется взять 
+                                                                        # несколько слов со следующей стркои
+                        future_row = next(open_file, None)
+                        if future_row is not None:
+                            future_index = abs(len(line) - (position + 5))
+                            result = " ".join(line[position - 5:] + (future_row.strip()).split()[:future_index])
+                            print(f'Найдено в файле: {args.path}',
+                                  f'Найдено на строке: {i}, Содержимое строки: {result}')
+                        else:
+                            result = " ".join(line[position - 5:])  # Если следующей строки нет
+                            print(f'Найдено в файле: {args.path}',
+                                  f'Найдено на строке: {i}, Содержимое строки: {result}')
+                    case _ if position > 4 and position + 5 <= len(line):  # Берем данные с одной строки
+                        result = " ".join(line[position - 5: position + 5])
                         print(f'Найдено в файле: {args.path}',
                               f'Найдено на строке: {i}, Содержимое строки: {result}')
-                    else:
-                        result = " ".join(line[position - 5:])
+                    case _ if position <= 4 and i < 2:  # Если не получится взять несколько слов с предыдущей строки
+                        result = " ".join(line[0: position + 5])
                         print(f'Найдено в файле: {args.path}',
                               f'Найдено на строке: {i}, Содержимое строки: {result}')
-                elif position > 4 and position + 5 <= len(line):
-                    result = " ".join(line[position - 5: position + 5])
-                    print(f'Найдено в файле: {args.path}',
-                          f'Найдено на строке: {i}, Содержимое строки: {result}')
-                elif position <= 4 and i < 2:
-                    result = " ".join(line[0: position + 5])
-                    print(f'Найдено в файле: {args.path}',
-                          f'Найдено на строке: {i}, Содержимое строки: {result}')
-                elif position <= 4 and i > 1:
-                    start_position = position - 5
-                    result = " ".join(previous_row[start_position:] + line[0: position + 5])
-                    print(f'Найдено в файле: {args.path}',
-                          f'Найдено на строке: {i}, Содержимое строки: {result}')
+                    case _ if position <= 4 and i > 1:  # Если надо получится несколько слов с предыдущей строки
+                        start_position = position - 5
+                        result = " ".join(previous_row[start_position:] + line[0: position + 5])
+                        print(f'Найдено в файле: {args.path}',
+                              f'Найдено на строке: {i}, Содержимое строки: {result}')
             except Exception as e:
                 print(f'Непредвиденная ошибка {e}')
             previous_row = line
